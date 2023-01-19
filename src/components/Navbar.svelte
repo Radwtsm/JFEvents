7883e26c508667390aee12e791fdfd20eb6392bc
radwtsm <radwtsm@gmail.com>
2023-01-19 14:47:31 +0100

    added component library and made all the main pages

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<Navbar let:hidden let:toggle color="none">
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl font-semibold text-white"> JF Events </span>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+		<NavLi href="/televisione">Televisione</NavLi>
+		<NavLi href="/cinema">Cinema</NavLi>
+		<NavLi href="/grandieventi">Grandi Eventi</NavLi>
+	</NavUl>
+</Navbar>
+
+<style>
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,18 @@
 <script>
-	import Header from './Header.svelte';
+	import '../app.postcss';
 	import './styles.css';
+	import Navbar from '../components/Navbar.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
-<div class="app">
-	<Header />
+<div class="app ">
+	<Navbar />
 
 	<main>
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	<Footer />
 </div>
 
 <style>
@@ -31,23 +31,5 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
